6c5b15e2765a641024f1d840e28a750c6122258c
lydia-yoons <87120715+lydia-yoons@users.noreply.github.com>
2021-07-08 18:08:48 +0900

    HelloJava update

---
+++ HelloJava.java
@@ -0,0 +1,20 @@
+package test;
+
+public class HelloJava {
+	public static void main(String[]args) {
+		System.out.println("HelloJava");
+		System.out.println("Hi~~~~");
+		
+		//10을 변수 value의 초기값으로 저장
+		int value = 10;
+		
+		//변수 value의 값을 일고 10을 더하는 산술 연산수행
+		//연산의 결과값을 변수 result의 초기값으로 저장
+		int result = value+10;
+		
+		//변수 result 값을 읽고 콘솔에 출력
+		System.out.println(result); 
+		
+	}
+
+}
